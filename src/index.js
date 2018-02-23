3eef0b8ca8d724d6f99938d525271f2189cc7724
St1ch <st1ch@mail.ru>
2018-02-23 23:30:56 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var count = 0, maxNod = 1;
+
+  for(var i = 2; i <= base; i++) {
+  	if(base % i === 0) {
+  		maxNod = i;
+  	}
+  }
+
+  while(number > 0){
+  	number = Math.floor( number / maxNod);
+  	count += number;
+  }
+
+  return count;
+
 }
